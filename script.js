841fd5f302fbd1ce2e1848d0895a8b3f359cdd5b
Shakya77 <bijanshakya145@gmail.com>
2025-01-16 11:51:06 +0545

    some changes made for the password used for to be stored

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const symbols = "`~!@#$%^&*()-_=+[{]}|;:',<.>/?";
 
 const allChars = upperCase + lowerCase + number + symbols;
 
-function createPassoword() {
+function createPassword() {
   let password = "";
   password += upperCase[Math.floor(Math.random() * upperCase.length)];
   password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
@@ -17,11 +17,52 @@ function createPassoword() {
   while (length > password.length) {
     password += allChars[Math.floor(Math.random() * allChars.length)];
   }
+  password = password
+    .split("")
+    .sort(() => Math.random() - 0.5)
+    .join("");
 
   passwordBox.value = password;
 }
 
-function copyPassowrd() {
-  passwordBox.select();
-  document.execCommand("copy");
+function openModal() {
+  document.getElementById("accountModal").style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("accountModal").style.display = "none";
+}
+
+function copyPassword() {
+  openModal();
+}
+
+function submitAccount() {
+  const account = document.getElementById("accountInput").value;
+  if (account) {
+    navigator.clipboard
+      .writeText(passwordBox.value)
+      .then(() => {
+        const alertDiv = document.createElement("div");
+        alertDiv.innerHTML = `Password copied for <b>${account}</b> to clipboard!`;
+        alertDiv.style.position = "fixed";
+        alertDiv.style.top = "10px";
+        alertDiv.style.right = "10px";
+        alertDiv.style.backgroundColor = "#019f55";
+        alertDiv.style.color = "white";
+        alertDiv.style.padding = "10px 20px";
+        alertDiv.style.borderRadius = "5px";
+        alertDiv.style.zIndex = "1000";
+        alertContainer.appendChild(alertDiv);
+
+        setTimeout(() => {
+          alertContainer.removeChild(alertDiv);
+        }, 3000); // Display alert for 3 seconds
+
+        closeModal(); // Close the modal
+      })
+      .catch((err) => alert("Failed to copy password: " + err));
+  } else {
+    alert("Please specify the account.");
+  }
 }
